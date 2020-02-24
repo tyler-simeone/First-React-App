f570167da9e5694451ef0809fdfa28220fcefcd6
Tyler Simeone <tyler.simeone@gmail.com>
2020-02-24 11:33:04 -0600

    updated animal tabs JS files to fetch data from API and render that data realtime with react

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+//import the components we will need
+import AnimalCard from './AnimalCard';
+import AnimalManager from '../../modules/AnimalManager';
 
 const AnimalList = () => {
-    return (
-        <div className="container-cards">
-        We'll put some animals here eventually...
-        </div>
-    );
-};
+  // The initial state is an empty array
+  const [animals, setAnimals] = useState([]);
 
-export default AnimalList;
+  const getAnimals = () => {
+    // After the data comes back from the API, we
+    //  use the setAnimals function to update state
+    return AnimalManager.getAll().then(animalsFromAPI => {
+      setAnimals(animalsFromAPI)
+    });
+  };
+
+  // got the animals from the API on the component's first render... useEffect waits for components to 'Mount' to the DOM and THEN
+  // fetches the API data and displays it. 
+  useEffect(() => {
+    getAnimals();
+  }, []);
+
+  // Finally we use map() to "loop over" the animals array to show a list of animal cards
+  return (
+    <div className="container-cards">
+      {animals.map(animal => <AnimalCard key={animal.id} animal={animal} />)}
+    </div>
+  );
+};
+export default AnimalList
